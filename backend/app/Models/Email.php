a5ba0c86de50df28f837acc6eb5975a75c7e5694
Kicovl13 <122311186+Kicovl13@users.noreply.github.com>
2024-10-18 19:15:05 -0600

    creacion de los modelos y migraciones en el backend

---
+++ backend/app/Models/Email.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Email extends Model
+{
+    public function contact()
+    {
+        return $this->belongsTo(Contact::class);
+    }
+
+}
